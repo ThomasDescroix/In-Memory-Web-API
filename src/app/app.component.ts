1ea8365ffba00b5d0d4c5d5564e5cd267599a6dd
ThomasDescroix <descroix.thomas@gmail.com>
2022-02-19 14:18:56 +0100

    🎉 create new Angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'imw-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'in-memory-web-api';
+}
